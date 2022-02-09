b4728014be47cb61652139442e984d25f28a0d16
simplesphere <anoop.jeewoolall@gmail.com>
2022-02-08 22:55:57 -0500

    updated eslintrc rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
   parserOptions: {
+    project: './tsconfig.json',
     ecmaVersion: 'latest',
     ecmaFeatures: {
       jsx: true,
@@ -27,6 +28,11 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:jest/recommended',
+    'plugin:jest/style',
+    'plugin:testing-library/react',
     'plugin:jsx-a11y/recommended',
     'plugin:prettier/recommended',
   ],
--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -1,1 +1,6 @@
-declare module '*.png'
+declare module '*.jpg' {
+  export default '' as string
+}
+declare module '*.png' {
+  export default '' as string
+}
